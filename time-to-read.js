1acb5033cabb7bd93c77cd7e5a9062a60a729db9
Eric Humpert <EricMac@Ricks-MBP.attlocal.net>
2016-04-16 14:09:50 -0500

    updated the calculation function with a correct one.

--- time-to-read.js
+++ time-to-read.js
@@ -1,11 +1,12 @@
 var words = "Walk on car leaving trail of paw prints on hood and windshield eat a plant, kill a hand, or eat and than sleep on your face ears back wide eyed. Eat a plant, kill a hand climb a tree, wait for a fireman jump to fireman then scratch his face yet human give me attention meow roll on the floor purring your whiskers off. Hack up furballs. Poop in the plant pot knock dish off table head butt cant eat out of my own dish see owner, run in terror eat the fat cats food sleep on dog bed, force dog to sleep on floor and lick the plastic bag. Jump off balcony, onto stranger's head scamper sun bathe, for scamper. Spit up on light gray carpet instead of adjacent linoleum run outside as soon as door open cat slap dog in face but chew foot poop in the plant pot. Get video posted to internet for chasing red dot. Touch water with paw then recoil in horror hide head under blanket so no one can see and meow all night having their mate disturbing sleeping humans and unwrap toilet paper scamper or hiss at vacuum cleaner so chase red laser dot. Chase after silly colored fish toys around the house vommit food and eat it again. Scream at teh bath. Climb leg pee in human's bed until he cleans the litter box scratch leg; meow for can opener to feed me gnaw the corn cob hide at bottom of staircase to trip human howl uncontrollably for no reason and intrigued by the shower. Use lap as chair hunt anything that moves the dog smells bad so stare at the wall, play with food and get confused by dust. Climb leg stare at the wall, play with food and get confused by dust. Knock dish off table head butt cant eat out of my own dish leave fur on owners clothes rub face on owner ears back wide eyed chirp at birds. Flop over hate dog, yet roll on the floor purring your whiskers off sit in box chase red laser dot but instantly break out into full speed gallop across the house for no reason. Sleep in the bathroom sink roll on the floor purring your whiskers off or toy mouse squeak roll over. Stare at ceiling light run in circles put toy mouse in food bowl run out of litter box at full speed so russian blue find empty spot in cupboard and sleep all day. Present belly, scratch hand when stroked. Thug cat gnaw the corn cob intrigued by the shower favor packaging over toy, or have secret plans. Eat owner's food toy mouse squeak roll over, destroy couch. Then cats take over the world sleep on keyboard, or soft kitty warm kitty little ball of furr. Immediately regret falling into bathtub stare out the window for then cats take over the world tuxedo cats always looking dapper see owner, run in terror for when in doubt, wash. ";
 
 
-function timetoread(string) {
-  var count = string.split(' ');
-  var time = (count.length / 230);
+function timetoread(str) {
+  console.log(str);
+  var count = str.split(' ');
   console.log(count.length);
-  return Math.round(time);
+  var time = Math.round(count.length / 230);
+  return time;
 }
 
 console.log(timetoread(words));
